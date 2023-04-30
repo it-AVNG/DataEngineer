c841901ba33ec9f893e798a4c4674879b5dcff34
it-AVNG <it.avng@gmail.com>
2023-04-30 13:02:20 +0700

    Created construct for Stack and Queue

---
+++ Stack_Queues/queue-construct.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        
+
+class Queue:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length =1
+
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length+=1
+        return True
+    
+    def dequeue(self,value):
+        if self.first is None:
+            return None
+        
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+          self.first = self.first.next
+          temp.next = None
+        self.length -=1
+
+        return temp
