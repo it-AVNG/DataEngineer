72cbedf7c74cddf93984973b24f0a93a003cd34a
it-AVNG <it.avng@gmail.com>
2023-05-03 18:31:49 +0700

    add the method for recursively insert, check value contains, delete for binary search tree

---
+++ recursive/factori.py
@@ -0,0 +1,7 @@
+def factorial(num):
+  if num ==1:
+    return 1
+  result = num * factorial(num-1)
+  return result
+
+print(factorial(5))
---
+++ recursive/rBST.py
@@ -0,0 +1,86 @@
+class Node:
+    def __init__(self,value):
+        self.value =value
+        self.left=None
+        self.right=None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+  
+    def __r_insert(self,current_node,value):
+        if current_node == None:
+            return Node(value)
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left,value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right,value)
+        return current_node
+         
+    def r_insert(self,value):
+        if self.root == None:
+            self.root = Node(value)
+        self.__r_insert(self.root,value)
+    
+    def __r_contains(self,current_node,value):
+        if current_node == None:
+            return False
+        if value == current_node.value:
+            return True
+        if value < current_node.value:
+          return self.__r_contains(current_node.left,value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right,value)
+        
+    def r_contains(self,value):
+        return self.__r_contains(self.root,value)   
+
+    def min_value(self,current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        
+        return current_node.value
+        
+        
+
+
+    def __delete_node(self, current_node, value):
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node.left,value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.left            
+            elif current_node.right == None:
+                current_node = current_node.right
+            
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)   
+        
+        
+        
+        return current_node
+        
+    def delete_node(self,value):
+        self.__delete_node(self.root,value)
+            
+
+my_tree = BinarySearchTree()
+my_tree.r_insert(47)
+my_tree.r_insert(21)
+my_tree.r_insert(76)
+my_tree.r_insert(18)
+my_tree.r_insert(27)
+my_tree.r_insert(52)
+my_tree.r_insert(82)
+
+print('BST contains 21:')
+print(my_tree.r_contains(21))
