17ed349f296878c0b3b1099018112853bcb6632e
it-AVNG <it.avng@gmail.com>
2023-05-05 05:39:37 +0700

    fix typos

--- BasicSort/buble.py
+++ BasicSort/buble.py
@@ -7,14 +7,6 @@ def bubble_sort(my_list):
                 my_list[j]=my_list[j+1]
                 my_list[j+1]=temp
         i-=1
-
-
-    # for i in range(len(my_list) - 1, 0 ,-1):
-    #     for j in range(i):
-    #         if my_list[j] > my_list[j+1]:
-    #             temp = my_list[j]
-    #             my_list[j] = my_list[j+1]
-    #             my_list[j+1] = temp
     return my_list
 
 my_list = [3, 7, 1, 9, 2, 6, 8, 4, 5, 10]
